9811247e2ed20869fac9fee699ab6a0719bc8d28
jds1985 <jimsaenz1985@yahoo.com>
2025-11-04 19:51:06 -0500

    Create Home component in index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '50px', textAlign: 'center' }}>
+      <h1>Welcome to Cipher</h1>
+      <p>The AI core is live and connected.</p>
+    </div>
+  );
+}
