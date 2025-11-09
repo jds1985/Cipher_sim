654b12e9811f64fff4fcd43be22f5636bf9eb4c6
jds1985 <jimsaenz1985@yahoo.com>
2025-11-09 07:36:18 -0500

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,4 @@
-// --- firebaseAdmin.js ---
-// This uses the Firebase Admin SDK for secure server access.
-
+// firebaseAdmin.js
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
@@ -8,10 +6,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   });
 }
 
-export const adminDB = admin.firestore();
-export const FieldValue = admin.firestore.FieldValue;
+const db = admin.firestore();
+export { db };
