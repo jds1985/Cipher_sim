b8161e88b6857c717a2ca87322787cccb982333e
jds1985 <jimsaenz1985@yahoo.com>
2025-11-11 12:00:45 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,15 @@
-// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiToKj76nxjFXWhLx9DS6VE8K86OFiq",
+  apiKey: "AIzaSyDiToKj76nxjfXWhLiXgDS6VE8K86OFfiQ",
   authDomain: "digisoul1111.firebaseapp.com",
   projectId: "digisoul1111",
   storageBucket: "digisoul1111.appspot.com",
   messagingSenderId: "260537897412",
-  appId: "1:260537897412:web:5c9cd6462747cde2c5491"
+  appId: "1:260537897412:web:5c9cd6462747cde2c5491",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
