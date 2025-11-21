f7b67d4c3b86ebabf044eb89d78112b001bc20a7
jds1985 <jimsaenz1985@yahoo.com>
2025-11-21 08:32:27 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  distDir: '.next'
+  reactStrictMode: false,
+  distDir: '.next',
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: "Cross-Origin-Resource-Policy", value: "cross-origin" },
+          { key: "Permissions-Policy", value: "camera=*, microphone=*, fullscreen=*" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "X-Content-Type-Options", value: "nosniff" }
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
