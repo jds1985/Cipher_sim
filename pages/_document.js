27ed527812a6f5876dec4f7f9b1a0d04283a5da6
jds1985 <jimsaenz1985@yahoo.com>
2025-11-22 08:56:03 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -8,16 +7,20 @@ export default function Document() {
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
 
-        {/* Icons */}
-        <link rel="icon" href="/icon-192.png" sizes="192x192" />
-        <link rel="icon" href="/icon-256.png" sizes="256x256" />
-        <link rel="icon" href="/icon-384.png" sizes="384x384" />
-        <link rel="icon" href="/icon-512.png" sizes="512x512" />
+        {/* App Icons */}
+        <link rel="icon" href="/icon-512.png" />
 
-        {/* Android splash fixes */}
+        {/* Theme */}
         <meta name="theme-color" content="#000000" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        {/* Camera + Microphone permissions */}
+        <meta
+          httpEquiv="Permissions-Policy"
+          content="camera=(self), microphone=(self)"
+        />
+
+        {/* Service worker */}
+        <link rel="serviceworker" href="/sw.js" />
       </Head>
 
       <body>
