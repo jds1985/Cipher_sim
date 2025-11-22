fd01d9024ca9277da7f855837a19be72a93bb038
jds1985 <jimsaenz1985@yahoo.com>
2025-11-22 08:30:27 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,23 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Manifest */}
+        {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
 
-        {/* Allow camera + mic + fullscreen */}
+        {/* Icons */}
+        <link rel="icon" href="/icon-192.png" sizes="192x192" />
+        <link rel="icon" href="/icon-256.png" sizes="256x256" />
+        <link rel="icon" href="/icon-384.png" sizes="384x384" />
+        <link rel="icon" href="/icon-512.png" sizes="512x512" />
+
+        {/* Android splash fixes */}
+        <meta name="theme-color" content="#000000" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-
-        {/* REQUIRED for Android camera access */}
-        <meta httpEquiv="Permissions-Policy" content="camera=(self), microphone=(self)" />
       </Head>
 
       <body>
