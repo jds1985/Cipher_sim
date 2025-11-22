af5b63d42c235a68b920fb4c965a0edba794c67b
jds1985 <jimsaenz1985@yahoo.com>
2025-11-22 09:04:30 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,18 @@
+// pages/_document.js
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
 
         {/* App Icons */}
         <link rel="icon" href="/icon-512.png" />
+        <link rel="apple-touch-icon" href="/icon-512.png" />
 
         {/* Theme */}
         <meta name="theme-color" content="#000000" />
