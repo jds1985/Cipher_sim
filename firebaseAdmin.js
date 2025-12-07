8b963401f97355a8eff378fcadbc94b1c4f296e3
jds1985 <jimsaenz1985@yahoo.com>
2025-12-07 01:25:55 -0500

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,12 +1,13 @@
-import admin from "firebase-admin";
+// firebaseAdmin.js
+import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      JSON.parse(
-        Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_B64, "base64").toString()
-      )
-    ),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
