0115b59dd6c288e910ad60c143b0d4a910f89e42
jds1985 <jimsaenz1985@yahoo.com>
2025-12-17 13:21:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
-    <div style={{ color: "white", padding: 40 }}>
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        minHeight: "100vh",
+        padding: 40,
+      }}
+    >
       <h1>INDEX IS RENDERING</h1>
     </div>
   );
