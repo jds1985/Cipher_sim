c84bfee65379366ddc5d391a2b9ae0a4a6b6e689
jds1985 <jimsaenz1985@yahoo.com>
2025-12-29 23:09:02 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-import ChatPanel from "../components/ChatPanel";
+import ChatPanel from "../components/chat/ChatPanel";
 
 export default function Home() {
   return (
-    <div style={{ height: "100vh", background: "#05050b" }}>
+    <div
+      style={{
+        height: "100vh",
+        background: "#05050b",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <ChatPanel />
     </div>
   );
