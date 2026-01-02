b2b31575d574241db8d80f9acab9b8b0ab9f4f61
jds1985 <jimsaenz1985@yahoo.com>
2026-01-02 13:09:27 -0500

    Update decipher.js

--- pages/api/decipher.js
+++ pages/api/decipher.js
@@ -1,9 +1,3 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    reply: "Decipher is alive."
-  });
-}
-
 import OpenAI from "openai";
 
 const openai = new OpenAI({
