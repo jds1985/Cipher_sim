5c455abcf76b3c8a3d0db267cda9fce781c836e3
jds1985 <jimsaenz1985@yahoo.com>
2026-01-06 20:39:45 -0500

    Refactor MessageList component to use bottomRef

--- components/chat/MessageList.jsx
+++ components/chat/MessageList.jsx
@@ -1,21 +1,13 @@
-"use client";
-
+// components/chat/MessageList.jsx
 import MessageBubble from "./MessageBubble";
 
-export default function MessageList({
-  messages,
-  onTouchStart,
-  onTouchEnd,
-}) {
+export default function MessageList({ messages, bottomRef }) {
   return (
-    <div
-      className="flex-1 overflow-y-auto p-4 flex flex-col gap-3"
-      onTouchStart={onTouchStart}
-      onTouchEnd={onTouchEnd}
-    >
-      {messages.map((msg, i) => (
-        <MessageBubble key={i} message={msg} />
+    <>
+      {messages.map((m, i) => (
+        <MessageBubble key={i} role={m.role} content={m.content} />
       ))}
-    </div>
+      <div ref={bottomRef} />
+    </>
   );
 }
