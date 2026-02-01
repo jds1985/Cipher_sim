590c1cd42c24faa43dfe72458c7d9a8fba8f8e34
jds1985 <jimsaenz1985@yahoo.com>
2026-01-31 22:01:07 -0500

    Update test-firebase.js

--- pages/api/test-firebase.js
+++ pages/api/test-firebase.js
@@ -1,26 +1,6 @@
-import admin from "firebase-admin";
-
 export default async function handler(req, res) {
-  try {
-    if (!admin.apps.length) {
-      admin.initializeApp({
-        credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-        }),
-      });
-    }
-
-    res.json({
-      success: true,
-      project: process.env.FIREBASE_PROJECT_ID,
-      email: process.env.FIREBASE_CLIENT_EMAIL,
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-      stack: err.stack,
-    });
-  }
+  res.json({
+    hasBase64: !!process.env.FIREBASE_ADMIN_BASE64,
+    hasPrivateKey: !!process.env.FIREBASE_PRIVATE_KEY,
+  });
 }
