4ab1c94ebf521a941333436a4ccf35457461b70d
jds1985 <jimsaenz1985@yahoo.com>
2026-02-01 21:05:21 -0500

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -2,11 +2,17 @@ import { runCipherCore } from "../../cipher_core/core.js";
 import { loadMemory } from "../../cipher_core/memory.js";
 import { runOrchestrator } from "../../cipher_os/runtime/orchestrator.js";
 
+import { loadMemoryNodes } from "../../cipher_os/memory/memoryGraph.js";
+
 export default async function handler(req, res) {
   const memoryData = await loadMemory("jim");
+  const nodes = await loadMemoryNodes("jim", 10);
 
   const executivePacket = await runCipherCore(
-    { history: memoryData.history || [] },
+    {
+      history: memoryData.history || [],
+      nodes
+    },
     { userMessage: "Hello", returnPacket: true }
   );
 
