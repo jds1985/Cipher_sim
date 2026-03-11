28951d306f8de81e161d616f22045fcf0a34ecda
jds1985 <jimsaenz1985@yahoo.com>
2026-03-10 21:42:51 -0400

    Create TypingText.js

---
+++ components/TypingText.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function TypingText({ text, speed = 18 }) {
+  const [displayed, setDisplayed] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+    setDisplayed("");
+
+    const interval = setInterval(() => {
+      i++;
+      setDisplayed(text.slice(0, i));
+      if (i >= text.length) clearInterval(interval);
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [text, speed]);
+
+  return <span>{displayed}</span>;
+}
