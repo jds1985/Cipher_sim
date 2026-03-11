8954715f2d343e768bac0304138423426e320eec
jds1985 <jimsaenz1985@yahoo.com>
2026-03-10 22:14:24 -0400

    Create TypingText.jsx

---
+++ components/chat/TypingText.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function TypingText({ text = "", speed = 12 }) {
+  const [displayed, setDisplayed] = useState("");
+
+  useEffect(() => {
+    setDisplayed("");
+    let i = 0;
+
+    const interval = setInterval(() => {
+      i++;
+      setDisplayed(text.slice(0, i));
+      if (i >= text.length) clearInterval(interval);
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [text, speed]);
+
+  return <span>{displayed}</span>;
+}
