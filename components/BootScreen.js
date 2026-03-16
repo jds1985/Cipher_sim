55a3c9a4d08dbd1309f36a3549812cf9ec9135ba
jds1985 <jimsaenz1985@yahoo.com>
2026-03-16 02:13:20 -0400

    Update BootScreen.js

--- components/BootScreen.js
+++ components/BootScreen.js
@@ -1,29 +1,38 @@
 import { useEffect, useState } from "react";
 
 export default function BootScreen({ onComplete }) {
-  const [active, setActive] = useState(false);
+  const [line, setLine] = useState("");
 
   useEffect(() => {
-    setTimeout(() => setActive(true), 200);
+    const steps = [
+      "Initializing Cipher Core...",
+      "Loading memory nodes...",
+      "Connecting models...",
+      "Cipher OS ready"
+    ];
 
-    const audio = new Audio("/sounds/power.mp3");
-    setTimeout(() => {
-      audio.play().catch(() => {});
-    }, 900);
+    let i = 0;
 
-    const timer = setTimeout(() => {
-      onComplete();
-    }, 2000);
+    const interval = setInterval(() => {
+      setLine(steps[i]);
+      i++;
 
-    return () => clearTimeout(timer);
+      if (i === steps.length) {
+        clearInterval(interval);
+        setTimeout(onComplete, 700);
+      }
+    }, 500);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="boot-container">
-      <img
-        src="/icons/cipher-512.png"
-        className={`boot-logo ${active ? "boot-active" : ""}`}
-      />
+      <img src="/icons/cipher-512.png" className="boot-logo" />
+
+      <div className="boot-text">
+        {line}
+      </div>
     </div>
   );
 }
