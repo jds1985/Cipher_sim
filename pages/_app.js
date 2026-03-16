2f649d680295fd496602cda3af7ba6a3239a772d
jds1985 <jimsaenz1985@yahoo.com>
2026-03-16 01:37:22 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
 import "../styles/cipher-theme.css";
+import "../styles/boot.css";
 
 import Head from "next/head";
+import { useState } from "react";
+import BootScreen from "../components/BootScreen";
 
 export default function MyApp({ Component, pageProps }) {
+  const [booted, setBooted] = useState(false);
+
+  if (!booted) {
+    return <BootScreen onComplete={() => setBooted(true)} />;
+  }
+
   return (
     <>
       <Head>
