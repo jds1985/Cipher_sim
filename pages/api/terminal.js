1840cca74228c2ffd58f92124928993e91e76923
jds1985 <jimsaenz1985@yahoo.com>
2026-09-04 15:14:44 -0400

    Delete pages/api/terminal.js

--- pages/api/terminal.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "METHOD_NOT_ALLOWED" });
-  }
-
-  return res.status(200).json({
-    status: "TERMINAL_ONLINE"
-  });
-}
